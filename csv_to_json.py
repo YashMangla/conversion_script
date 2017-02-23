cb7a8c040532ce3ce0dc4098ab17b94373a2f359
Yash Mangla <mangla@MANGLA1.asiapacific.hpqcorp.net>
2017-02-23 16:18:31 +0530

    add csv to json converting script with some type changes in old files

--- csv_to_json.py
+++ csv_to_json.py
@@ -1,28 +1,30 @@
 # Copyright (c) 2017, Yash Mangla and Contributors
 # MIT License. See license.txt
 '''
-    convert tab separated file to JSON file
+    convert comma's separated file to JSON file
 '''
 
-import csv, json
+import csv
+import json
 
 def tsv_to_json(inp, out):
     arr = []
     outFile = open(out, 'wb')
-    with open(inp, 'rb') as tsvin:
-        tsvin = csv.reader(tsvin, delimiter='\t')
-        keys = [i for i in tsvin.next()]
+        # here i opened file in Universal mode you can change this according to your requirment.
+    with open(inp, 'rU') as csvin:
+        csvin = csv.reader(csvin, delimiter=',')
+        keys = [i for i in csvin.next()]
         print keys
-        for row in tsvin:
+        for row in csvin:
             x={}
             for i in range(0, len(row)):
                 x.setdefault(keys[i], row[i])
             arr.append(x)
-    
+
     #write output in json file
     json.dump(arr, outFile)
 
 if __name__ == "__main__":
-    inp = "data.tsv"
-    out = "data.json"
+    inp = "sample.csv"
+    out = "sample.json"
     tsv_to_json(inp, out)
